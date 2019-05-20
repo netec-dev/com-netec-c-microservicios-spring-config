79a04056b5ce211cdf717ccaad2bd8fd14c72be5
Pedro Cazu <training@pcabrera.com>
2019-05-20 16:58:32 -0500

    Spring Cloud Bus Demo

--- com-netec-c-microservicios-spring-cloud-config-servidor/src/main/java/com/netec/c/microservicios/spring/cloud/config/SCConfigServidorApp.java
+++ com-netec-c-microservicios-spring-cloud-config-servidor/src/main/java/com/netec/c/microservicios/spring/cloud/config/SCConfigServidorApp.java
@@ -2,14 +2,28 @@ package com.netec.c.microservicios.spring.cloud.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.monitor.GithubPropertyPathNotificationExtractor;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigServer
 @EnableDiscoveryClient
 public class SCConfigServidorApp {
-  public static void main(String[] args) {
-	SpringApplication.run(SCConfigServidorApp.class, args);
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(SCConfigServidorApp.class, args);
+	}
+
+	@Bean
+//	@ConditionalOnProperty(value = "spring.cloud.config.server.monitor.github.enabled", havingValue = "true", matchIfMissing = true)
+	public GithubPropertyPathNotificationExtractor githubPropertyPathNotificationExtractor() {
+		return new GithubPropertyPathNotificationExtractor();
+	}
+
+//	@Bean
+//	public GithubPropertyPathNotificationExtractor githubPropertyPathNotificationExtractor() {
+//	  return new GithubPropertyPathNotificationExtractor();
+//	}
 }
